1a4c326ff9b1a0f60f249ce05a760349ff6b2ffc
Ali A <bakarow12@gmail.com>
2022-05-14 14:35:50 +0100

    Displaying errors

--- index.php
+++ index.php
@@ -3,40 +3,21 @@
 require 'vendor/autoload.php';
 
 // Create new app instance 
-$app = new \Slim\App;
+$app = new \Slim\App([
+    'settings' => [
+        'displayErrorDetails' => true,
+    ]
+]);
 
 // we can store items in the container 
 // it uses singleton pattern! meaning 
 // we are not reinstantiating this every time 
 $container = $app->getContainer();
 
-// add item to the container 
-$container['greeting'] = function (){
-    echo 'HI <br>';
-    return 'Hello from the container <br>';
-    // for example db
-    // return new PDO('...');
-};
 
-// for example db
-$container['db'] = function (){
-    // return new PDO('...');
-
-};
-
-// When someone lands on '/' we 
-// Call back function or a closure 
 $app->get('/', function(){
-    echo $this->greeting;
-    echo $this->greeting;
-    echo $this->greeting;
-    return 'Home Page <br>';
+    echo $this->something;
 });
-
-$app->get('/contact', function(){
-    return 'Contact';
-});
-
 
 $app->run();
 // phpinfo(); 
--- notes.md
+++ notes.md
@@ -2,9 +2,29 @@ git branch new_branh_name
 git branch --list
 git branch 
 git checkout branch_name
+git push --set-upstream origin 2_first_route
+
 
 create branch and swtich to it 
     - git checkout -b name
     - 
 
+Displaying errors:
+in the container class:
+    $app = new \Slim\App([
+        'settings' => [
+            'displayErrorDetails' => true,
+        ]
+    ]);
+More options:
+
+$defaultSettings = [
+        'httpVersion' => '1.1',
+        'responseChunkSize' => 4096,
+        'outputBuffering' => 'append',
+        'determineRouteBeforeAppMiddleware' => false,
+        'displayErrorDetails' => false,
+        'addContentLengthHeader' => true,
+        'routerCacheFile' => false,
+    ];
 
