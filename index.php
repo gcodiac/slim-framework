76c542a2c17eb1bbdf40cf0f2586496e735990ce
Ali A <bakarow12@gmail.com>
2022-05-14 14:18:46 +0100

    Containers and notes file

--- index.php
+++ index.php
@@ -2,10 +2,42 @@
 
 require 'vendor/autoload.php';
 
+// Create new app instance 
 $app = new \Slim\App;
 
+// we can store items in the container 
+// it uses singleton pattern! meaning 
+// we are not reinstantiating this every time 
+$container = $app->getContainer();
+
+// add item to the container 
+$container['greeting'] = function (){
+    echo 'HI <br>';
+    return 'Hello from the container <br>';
+    // for example db
+    // return new PDO('...');
+};
+
+// for example db
+$container['db'] = function (){
+    // return new PDO('...');
+
+};
+
+// When someone lands on '/' we 
+// Call back function or a closure 
 $app->get('/', function(){
-    return 'Bismillah';
+    echo $this->greeting;
+    echo $this->greeting;
+    echo $this->greeting;
+    return 'Home Page <br>';
 });
 
+$app->get('/contact', function(){
+    return 'Contact';
+});
+
+
 $app->run();
+// phpinfo(); 
+
---
+++ notes.md
@@ -0,0 +1,10 @@
+git branch new_branh_name
+git branch --list
+git branch 
+git checkout branch_name
+
+create branch and swtich to it 
+    - git checkout -b name
+    - 
+
+
